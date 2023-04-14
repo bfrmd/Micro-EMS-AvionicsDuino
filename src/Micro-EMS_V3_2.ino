b537643be1140e30a854404d850617a6c7699ae6
bfrmd <58253078+bfrmd@users.noreply.github.com>
2023-04-14 18:17:38 +0200

    Update Micro-EMS_V3_2.ino

--- src/Micro-EMS_V3_2.ino
+++ src/Micro-EMS_V3_2.ino
@@ -1,4 +1,31 @@
-
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                                                 Micro-EMS Avionicsduino V 3.2
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Micro-EMS Avionicsduino V 3.2 is free software    
+  MIT License (MIT)
+  
+  Copyright (c) 2023 AvionicsDuino - benjamin.fremond@avionicsduino.com
+  https://avionicsduino.com/index.php/en/teensy-micro-ems/?preview=true
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+  of the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+    
+ *****************************************************************************************************************************/ 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------
 //                                                Connexions physiques des différents composants avec la carte Teensy 4.0
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------
